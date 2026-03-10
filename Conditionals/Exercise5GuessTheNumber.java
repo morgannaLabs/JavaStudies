9023483f84b41835013ec72ef332cd1b3d63b9ba
Nathalia Molina (Morganna) <141174383+morgannaLabs@users.noreply.github.com>
2026-03-10 18:45:29 -0300

    Create Exercise5GuessTheNumber game class
    Added a new Java class for a number guessing game.

---
+++ Conditionals/Exercise5GuessTheNumber.java
@@ -0,0 +1,16 @@
+/** Exercise 5 — Guess the Number Game
+ 
+The program picks a random number between 1 and 20. The user tries to guess it.
+Limit the user to 7 attempts.
+ 
+ by morgannaLabs - https://github.com/morgannaLabs
+ */
+
+public class Exercise5GuessTheNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
