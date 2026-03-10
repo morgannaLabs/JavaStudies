8d6966e5d20b2fd45733cb2070f26cb28bac13d3
Nathalia Molina (Morganna) <141174383+morgannaLabs@users.noreply.github.com>
2026-03-10 17:48:14 -0300

    Add Exercise3LargestNumber to find largest number
    Implemented a program to find the largest number in an array of five integers.

---
+++ Arrays/Exercise3LargestNumber.java
@@ -0,0 +1,32 @@
+/** Exercise 3 — Find the Largest Number
+Create an array with five integers and find the largest number in the array.
+by morgannaLabs - https://github.com/morgannaLabs
+*/
+
+public class Exercise3LargestNumber {
+	
+	public static void main (String [] args) {
+
+		int [] array = new int [5];
+		array[0] = 1;
+		array[1] = 9;
+		array[2] = 2;
+		array[3] = 15;
+		array[4] = 6;
+		
+		int largest = array[0];
+		
+		for (int value : array) {
+			
+			if (value > largest) {
+				
+				largest = value;
+			}
+			
+		}
+		
+		System.out.println("The largest number is: " + largest);
+		
+	}
+
+}
