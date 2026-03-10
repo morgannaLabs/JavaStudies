c986b2a0f8d1a178d0908f70a01bc7cbbe48c7b0
Nathalia Molina (Morganna) <141174383+morgannaLabs@users.noreply.github.com>
2026-03-10 18:44:59 -0300

    Add Exercise4TemperatureConverter class
    Implement a temperature converter that converts between Celsius and Fahrenheit based on user input.

---
+++ Conditionals/Exercise4TemperatureConverter.java
@@ -0,0 +1,18 @@
+/** Exercise 4 — Student Grade Evaluator
+ 
+Read a temperature and a scale (C or F). Convert it to the other scale.
+
+If C, convert to Fahrenheit, If F, convert to Celsius
+If the scale is invalid, display Invalid scale.
+ 
+ by morgannaLabs - https://github.com/morgannaLabs
+ */
+
+public class Exercise4TemperatureConverter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
