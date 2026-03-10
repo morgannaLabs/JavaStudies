c71a66d0519f73486a63154066df2195eda859f7
Nathalia Molina (Morganna) <141174383+morgannaLabs@users.noreply.github.com>
2026-03-10 18:43:43 -0300

    Create Exercise2GradeEvaluator class
    Implement a grade evaluator class with a header comment.

---
+++ Conditionals/Exercise2GradeEvaluator.java
@@ -0,0 +1,11 @@
+/** Exercise 2 — Student Grade Evaluator
+ 
+Read three grades of a student and calculate the average. Then classify the result as passed, recovery, failed. 
+Don't let invalid grade be inserted (less than 0 or greater than 10)
+ 
+ by morgannaLabs - https://github.com/morgannaLabs
+ */
+
+public class Exercise2GradeEvaluator {
+
+}
