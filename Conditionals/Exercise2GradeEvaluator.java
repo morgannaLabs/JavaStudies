61a2cc95807f9d5ef173a9a159c0466497973ce6
Nathalia Molina (Morganna) <141174383+morgannaLabs@users.noreply.github.com>
2026-03-20 15:34:31 -0300

    Implement grade evaluation and classification logic

--- Conditionals/Exercise2GradeEvaluator.java
+++ Conditionals/Exercise2GradeEvaluator.java
@@ -6,6 +6,64 @@ Don't let invalid grade be inserted (less than 0 or greater than 10)
  by morgannaLabs - https://github.com/morgannaLabs
  */
 
+
+import javax.swing.JOptionPane;
+
 public class Exercise2GradeEvaluator {
+	
+	static double grade1, grade2, grade3, average;
+	
+	public static void main (String [] args) {
+		
+		grade1 = readGrades(1);
+		grade2 = readGrades(2);
+		grade3 = readGrades(3);
+		
+		average = calculateAverage(grade1, grade2, grade3);
+		classifyFinalGrade(average);
+		
+	}
+	
+	public static double readGrades(int counter) {
+		
+		while (true) {
+		
+		try {
+		double grade = Double.parseDouble(JOptionPane.showInputDialog("Inform the " + counter + "º grade"));
+		return grade;
+		
+		}
+		
+		catch (NumberFormatException e) {
+
+		    JOptionPane.showMessageDialog(null, "Only Numbers between 0 and 10! Try Again!");
+		}
+	}
+		
+}
+	
+	public static double calculateAverage(double grade1, double grade2, double grade3) {
+		
+		double sum = grade1 + grade2 + grade3;
+		double average = sum / 3;
+		
+		return average;
+	}
+	
+	public static void classifyFinalGrade(double average) {
+		
+		if (average >= 7) {
+			JOptionPane.showMessageDialog(null, "Congratualtions, you passed!! Your final grade is: " + average);
+		}
+		
+		else if (average >= 5 && average <= 6) {
+			JOptionPane.showMessageDialog(null, "You must do the recovery! Your final grade is: " + average);
+		}
+		
+		else {
+			JOptionPane.showMessageDialog(null, "You failed! See Yo next year! Your final grade is: " + average);
+		}
+		
+	}
 
 }
